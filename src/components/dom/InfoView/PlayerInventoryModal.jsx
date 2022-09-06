c1f7e19c9106105c33991b3ae07bad78dc48a299
Nicholas Marino <nicholas.marino@zero.nyc>
2022-09-05 20:11:40 -0400

    lol

--- src/components/dom/InfoView/PlayerInventoryModal.jsx
+++ src/components/dom/InfoView/PlayerInventoryModal.jsx
@@ -5,17 +5,7 @@ export default function PlayerInventoryModal({ player }) {
   return (
     <div>
       <ModalTrigger trigger={"Player Inventory"} />
-      <div>
-        {player.inventory.map(item => 
-          <div>
-            <h2>{item.name} — {item.slot}</h2>
-            <p>{item.description}</p>
-            <ul>
-             {/* {item.effects.map(effect=> <li>{effect.skill} — {effect.value}</li>)}    */}
-            </ul>
-          </div>
-        )}
-      </div>
+
     </div>
   )
 }
